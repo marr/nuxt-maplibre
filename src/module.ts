ca4d1a3b833b1b805b25175d07a4ca27b9e0a102
David Marr <david.marr@contextlabs.com>
2025-06-15 14:25:50 -0400

    chore: Lint files on save

--- src/module.ts
+++ src/module.ts
@@ -3,7 +3,7 @@ import {
   addComponent,
   addImports,
   addTemplate,
-} from "@nuxt/kit";
+} from '@nuxt/kit'
 
 // Module options TypeScript interface definition
 // eslint-disable-next-line
@@ -11,92 +11,90 @@ export interface ModuleOptions {}
 
 // Components to export
 export const components = [
-  "MglMap",
-  "MglAttributionControl",
-  "MglCustomControl",
-  "MglFullscreenControl",
-  "MglGeolocateControl",
-  "MglNavigationControl",
-  "MglScaleControl",
-  "MglLogoControl",
-  "MglMarker",
-  "MglPopup",
-  "MglCanvasSource",
-  "MglGeoJsonSource",
-  "MglImageSource",
-  "MglRasterSource",
-  "MglRasterDemSource",
-  "MglVectorSource",
-  "MglVideoSource",
-  "MglBackgroundLayer",
-  "MglCircleLayer",
-  "MglFillLayer",
-  "MglFillExtrusionLayer",
-  "MglHeatmapLayer",
-  "MglHillshadeLayer",
-  "MglLineLayer",
-  "MglRasterLayer",
-  "MglSymbolLayer",
-];
+  'MglMap',
+  'MglAttributionControl',
+  'MglCustomControl',
+  'MglFullscreenControl',
+  'MglGeolocateControl',
+  'MglNavigationControl',
+  'MglScaleControl',
+  'MglLogoControl',
+  'MglMarker',
+  'MglPopup',
+  'MglCanvasSource',
+  'MglGeoJsonSource',
+  'MglImageSource',
+  'MglRasterSource',
+  'MglRasterDemSource',
+  'MglVectorSource',
+  'MglVideoSource',
+  'MglBackgroundLayer',
+  'MglCircleLayer',
+  'MglFillLayer',
+  'MglFillExtrusionLayer',
+  'MglHeatmapLayer',
+  'MglHillshadeLayer',
+  'MglLineLayer',
+  'MglRasterLayer',
+  'MglSymbolLayer',
+]
 
 // Composition functions to export
 export const composables = [
-  "useMap",
-  "useControl",
-  "useDisposableLayer",
-  "useLayer",
-  "usePositionWatcher",
-  "useSource",
-];
+  'useMap',
+  'useControl',
+  'useDisposableLayer',
+  'useLayer',
+  'usePositionWatcher',
+  'useSource',
+]
 
 export default defineNuxtModule<ModuleOptions>({
   meta: {
-    name: "@dmarr/nuxt-maplibre",
-    configKey: "maplibre",
+    name: '@dmarr/nuxt-maplibre',
+    configKey: 'maplibre',
     compatibility: {
-      nuxt: ">=3.0.0",
+      nuxt: '>=3.0.0',
     },
   },
   // Default configuration options of the Nuxt module
   defaults: {},
   async setup(options, nuxt) {
-
-
     // Auto-import vue-maplibre-gl components
     for (const component of components) {
       addComponent({
         name: component,
         export: component,
-        filePath: "@indoorequal/vue-maplibre-gl",
+        filePath: '@indoorequal/vue-maplibre-gl',
         chunkName: `nuxt-maplibre/${component}`,
-        mode: "all",
-      });
+        mode: 'all',
+      })
     }
 
     // Auto-import vue-maplibre-gl composables
     for (const composable of composables) {
       addImports({
         name: composable,
-        as: "useMgl" + composable.slice(3),
-        from: "@indoorequal/vue-maplibre-gl",
-      });
+        as: 'useMgl' + composable.slice(3),
+        from: '@indoorequal/vue-maplibre-gl',
+      })
     }
 
     const template = addTemplate({
-      filename: "assets/styles.css",
+      filename: 'assets/styles.css',
       getContents: () => {
         return `.maplibregl-map {
           --ml-font-icons: maplibregl-icons-lucide;
-        }`;
+        }`
       },
-    });
+    })
 
     // Add CSS using mapblibre-theme. It is much smaller than maplibre-gl.css
     // and has a more modern look. Add the change needed to use lucide icons.
     nuxt.options.css.push(
-      "maplibre-theme/icons.lucide.css",
-      "maplibre-theme/modern.css",
-      template.dst
-    );
+      'maplibre-theme/icons.lucide.css',
+      'maplibre-theme/modern.css',
+      template.dst,
+    )
   },
-});
+})
